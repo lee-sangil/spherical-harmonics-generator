797bec629cf8e7f47f6cada0d3db5b8c197852d9
Sangil Lee <sangillee724@gmail.com>
2024-12-22 11:59:06 +0900

    chore/remove margin of dat.gui

--- main.js
+++ main.js
@@ -62,6 +62,7 @@ const setting = {
 };
 
 const gui = new dat.GUI();
+gui.domElement.style.marginRight = '0px';
 gui.add(setting, 'mode', {'Red': 'red', 'Green': 'green', 'Blue': 'blue', 'Mono': 'mono'}).name('Mode').listen().onChange(updateMode);
 
 gui.add(setting, 'reset').name('Reset');
